8a255fa83c9ba1d58c54c969e9d0a31c15e4c3c6
ErtelDG <ertel.developer@t-online.de>
2023-02-02 22:40:52 +0100

    secureKeyAndDatabase

--- management_app_main/settings.py
+++ management_app_main/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6nbv*6r9t3=qcu0qqx8$90e%y^1t=m^*xn%s4y3rr%6nt%pw*f'
+SECRET_KEY = os.environ.get('secretKey')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
