ad60eaf3de721c1ede9c6a6956d447e161e2f122
ErtelDG <ertel.developer@t-online.de>
2023-02-04 12:17:55 +0100

    loginLogOutDone

--- management_app_main/urls.py
+++ management_app_main/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('kanban_board/', include('apps.kanban_board.urls')),
     path('admin/', admin.site.urls),
+    path("accounts/", include("django.contrib.auth.urls")),
+    path('index/', TemplateView.as_view(template_name='main/main.html'), name='main'),
+    path('', TemplateView.as_view(template_name='main/main.html'), name='main'),
 ]
